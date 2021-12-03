9c3b572a810f02744619f24f17c5a2f92289dc85
vuquangphong <thapquanviet@gmail.com>
2021-12-03 17:39:09 +0700

    [Phong] responsive... input citizen...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchCitizen from "./SearchCitizen";
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home">HOME home HOME home HOME</div>
+      <div>
+        <Link to='/input-citizen'>
+          <button>Nhập thông tin công dân</button>
+        </Link>
+      </div>
     </div>
   );
 };
