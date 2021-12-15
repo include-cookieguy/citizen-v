c5691556aba0b385fe51cafe534fc794315c3edd
Tuanndq <quangtuanxyz@gmail.com>
2021-12-16 01:43:49 +0700

    update unit status

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ChartCitizen from "../components/ChartCitizen";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Monitor from "../components/Monitor";
+import { getUnit } from "../redux/actions/unitAction";
 
 const Home = () => {
   const { auth } = useSelector((state) => state);
-
+  const dispatch = useDispatch()
+  let user = useSelector(state => state.auth.user)
+  useEffect(() => {
+    console.log(user.username)
+    dispatch(getUnit(user))
+  }, [])
   return (
     <div className="home-container">
       <Monitor />
