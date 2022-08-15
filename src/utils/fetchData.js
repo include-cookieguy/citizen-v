ab6186f7324c4c0ea683a8fc00d058afa03d226b
Thang <qthang1310@gmail.com>
2022-08-16 01:21:51 +0700

    add

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,50 +1,62 @@
 import axios from "axios";
-import { refreshToken } from '../redux/actions/authAction'
+import { refreshToken } from "../redux/actions/authAction";
 
-axios.interceptors.request.use(function (req) {
-  const token = localStorage.getItem('token')
-  Object.assign(req.headers, { 'Authorization': token })
-  return req;
-}, function (error) {
-  return Promise.reject(error);
+const baseURL = "https://citizen-v-api-v1.herokuapp.com";
+
+const axiosAuth = axios.create({
+  baseURL: baseURL,
 });
 
-axios.interceptors.response.use(function (res) {
-  return res;
-}, async function (error) {
-  if (error.response.status === 401) {
-    await refreshToken()
-    error.config.headers['Authorization'] = localStorage.getItem('token')
-    return axios.request(error.config)
-  } else if (error.response.status === 402) {
-    localStorage.removeItem('token')
-    localStorage.removeItem('firstLogin')
-    window.location.replace('/')
+axiosAuth.interceptors.request.use(
+  function (req) {
+    const token = localStorage.getItem("token");
+    Object.assign(req.headers, { Authorization: token });
+    return req;
+  },
+  function (error) {
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
+);
+
+axiosAuth.interceptors.response.use(
+  function (res) {
+    return res;
+  },
+  async function (error) {
+    if (error.response.status === 401) {
+      await refreshToken();
+      error.config.headers["Authorization"] = localStorage.getItem("token");
+      return axios.request(error.config);
+    } else if (error.response.status === 402) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("firstLogin");
+      window.location.replace("/");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export const getDataAPI = async (url) => {
-  const res = await axios.get(`/api/${url}`);
+  const res = await axiosAuth.get(`/api/${url}`);
   return res;
 };
 
 export const postDataAPI = async (url, post) => {
-  const res = await axios.post(`/api/${url}`, post);
+  const res = await axiosAuth.post(`/api/${url}`, post);
   return res;
 };
 
 export const putDataAPI = async (url, post) => {
-  const res = await axios.put(`/api/${url}`, post);
+  const res = await axiosAuth.put(`/api/${url}`, post);
   return res;
 };
 
 export const patchDataAPI = async (url, post) => {
-  const res = await axios.patch(`/api/${url}`, post);
+  const res = await axiosAuth.patch(`/api/${url}`, post);
   return res;
 };
 
 export const deleteDataAPI = async (url) => {
-  const res = await axios.delete(`/api/${url}`);
+  const res = await axiosAuth.delete(`/api/${url}`);
   return res;
 };
